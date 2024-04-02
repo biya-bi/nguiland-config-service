1562dcad4c44800eca56141101af6513a0832bc6
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-04-01 20:09:29 -0400

    Add licensing-service datasource configs

--- src/main/java/com/optimagrowth/configserver/ConfigurationServerApplication.java
+++ src/main/java/com/optimagrowth/configserver/ConfigurationServerApplication.java
@@ -1,14 +1,20 @@
 package com.optimagrowth.configserver;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+
+import com.optimagrowth.configserver.util.EncryptKeySetter;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigurationServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		EncryptKeySetter.set();
+
 		SpringApplication.run(ConfigurationServerApplication.class, args);
 	}
 
